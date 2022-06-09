143d11af135403480819584bf32a48baa1404348
Lucas Caminos <lucascaminosispc@gmail.com>
2022-06-08 22:58:17 -0300

    función suma

--- A3G3.py
+++ A3G3.py
@@ -2,9 +2,11 @@
 
 from bienvenida import bienvenida
 from ing2i import ing2i 
+from suma import suma
+
 
 bienvenida()
 nros_enteros = ing2i()
 print("Los números ingresados son:", nros_enteros) #imprime la lista en la que se almacenaron los números
-
+print("Suma= ", suma())
 
--- suma.py
+++ suma.py
@@ -1,11 +1,15 @@
 # Función suma, retorna la suma de 2 parámetros.
 
-def suma(num1, num2):
+def suma():
+    print("***Función suma***")
+    num1 = input("ingrese un numero\n")
+    num2 = input("ingrese otro numero\n")
+    return num1 + num2
+ 
+ 
+ 
+   
 
-    print(int(num1)+ int(num2))
-
-
-suma(num1 = input("ingrese un numero\n"), num2 = input("ingrese otro numero\n"))
 
 
 
