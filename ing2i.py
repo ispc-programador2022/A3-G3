656120685564ab5ed69d040917419e2f9cdbe743
ExComando <79213925+ExComando@users.noreply.github.com>
2022-06-08 21:44:09 -0300

    Add files via upload

---
+++ ing2i.py
@@ -0,0 +1,8 @@
+# Crear la función ing2i que permita ingresar dos valores enteros
+
+def ing2i():
+
+ num1 = input("Ingresa un numero entero\n")
+ num2 = input("Ingresa otro numero entero\n")
+
+ing2i()
