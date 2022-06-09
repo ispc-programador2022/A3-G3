372e6649ca48c1046710e372b7f8d938730f720a
Lucas Caminos <lucascaminosispc@gmail.com>
2022-06-08 23:28:14 -0300

    función ing2s

--- A3G3.py
+++ A3G3.py
@@ -3,9 +3,10 @@
 from bienvenida import bienvenida
 from ing2i import ing2i 
 from suma import suma
+from ing2s import ing2s
 
 
 bienvenida()
-print("Los números ingresados son:", ing2i()) #imprime la lista en la que se almacenaron los números
+print("Los números ingresados son:", ing2i()) 
 print("Suma= ", suma())
-
+print("Cadenas= ",ing2s())
---
+++ ing2s.py
@@ -0,0 +1,5 @@
+def ing2s ():
+    print("\n***Función Ing2i***")
+    s1=input("Ingrese el 1er valor string: ")
+    s2=input("Ingrese el 2er valor string: ")
+    return [s1,s2]  #devuelve el resultado en una lista
