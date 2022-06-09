afc0f0a14268fae7cff60512738ad506ef19971d
Lucas Caminos <lucascaminosispc@gmail.com>
2022-06-08 22:40:21 -0300

    .

--- A3G3.py
+++ A3G3.py
@@ -1,5 +1,8 @@
 #A3G3
 
-from Bienvenida import bienvenida
+from bienvenida import bienvenida
+from ing2i import ing2i 
 
 bienvenida()
+nros_enteros = ing2i()
+print("Los números ingresados son:", nros_enteros) #imprime la lista en la que se almacenaron los números
