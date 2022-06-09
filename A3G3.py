af6b3be913a3ad7e0b19aa5a51e0db06eec42309
Camilo Sanchez <camilo_sanchezcjs29@hotmail.com>
2022-06-09 19:27:16 -0300

    Actualizando el main

--- A3G3.py
+++ A3G3.py
@@ -1,26 +1,27 @@
-#A3G3
+# A3G3
 
 from Funciones.bienvenida import bienvenida
-from Funciones.ing2i import ing2i 
+from Funciones.ing2i import ing2i
 from Funciones.suma import suma
 from Funciones.ing2s import ing2s
 from Funciones.resta import resta
 from Funciones.producto import producto
 
-#Bienvenida
+# Bienvenida
 bienvenida()
 
-#Ingreso de dos valores enteros y asignación a la variable 'nums'
+# Ingreso de dos valores enteros y asignación a la variable 'nums'
 nums = ing2i()
 
-#Operación suma
+# Operación suma
 print("Suma = ", suma(nums[0], nums[1]))
 
-#Operación resta
-resta()
+# Operación resta
+print("Resta = ", resta(nums[0], nums[1]))
 
-#Ingreso de dos valores string
-print("Cadenas= ",ing2s())
+# Operacion producto
+print("Producto = ", producto(nums[0], nums[1]))
 
-#Operacion producto
-producto()
+# Ingreso de dos valores string
+print("Cadenas= ", ing2s())
+
