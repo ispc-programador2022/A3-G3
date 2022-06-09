2276754ea8cea496ca3dc8afd131bf43cefc7434
Lucas Caminos <lucascaminosispc@gmail.com>
2022-06-08 22:23:21 -0300

    func. ing2i

--- ing2i.py
+++ ing2i.py
@@ -3,5 +3,5 @@
 def ing2i():
     a=int(input("Ingrese el 1er valor entero: "))
     b=int(input("Ingrese el 2do valor entero: "))
-    return [a,b]
+    return [a,b]   #devuelve el resultado en una lista
     
