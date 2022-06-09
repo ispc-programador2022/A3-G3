85ba5700f77402649400b15ab91dbb39f35a1446
Camilo Sanchez <camilo_sanchezcjs29@hotmail.com>
2022-06-09 14:14:55 -0300

    Mejor distribución del main

--- A3G3.py
+++ A3G3.py
@@ -6,9 +6,17 @@ from Funciones.suma import suma
 from Funciones.ing2s import ing2s
 from Funciones.resta import resta
 
-
+#Bienvenida
 bienvenida()
-print("Los números ingresados son:", ing2i()) 
-print("Suma= ", suma())
-print("Cadenas= ",ing2s())
+
+#Ingreso de dos valores enteros y asignación a la variable 'nums'
+nums = ing2i()
+
+#Operación suma
+print("Suma = ", suma(nums[0], nums[1]))
+
+#Operación resta
 resta()
+
+#Ingreso de dos valores string
+print("Cadenas= ",ing2s())
