95b12d3e2dc451dad2f7b0d6fc77aa5a41bf2f88
ExComando <dcalla2885@gmail.com>
2022-06-09 21:04:26 +0000

    Carpeta Funciones

---
+++ Funciones/producto.py
@@ -0,0 +1,9 @@
+#Retorna el producto de dos parámetros
+print("\n***Función producto***\n")
+
+def producto(num1, num2):
+
+ print(int(num1) * int(num2))
+
+
+producto(num1=input("Ingrese un numero\n"), num2=input("Ingrese otro numero\n"))
