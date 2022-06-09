a182b4cb7d7e62eb1a5170bada2102bcfa08a53d
ExComando <dcalla2885@gmail.com>
2022-06-09 20:43:40 +0000

    first commit

---
+++ Funciones/resta.py
@@ -0,0 +1,9 @@
+#Función resta, retorna la resta de 2 parámetros
+print("\n***Función resta***\n")
+
+def resta(num1, num2):
+
+ print(int(num1) - int(num2))
+
+
+resta(num1=input("Ingrese un numero\n"), num2=input("Ingrese otro numero\n"))
