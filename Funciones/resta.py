eac6b23220247d53d6d74928c3d14fbe07275f4a
Camilo Sanchez <camilo_sanchezcjs29@hotmail.com>
2022-06-09 19:26:36 -0300

    Cambio en la función Resta

--- Funciones/resta.py
+++ Funciones/resta.py
@@ -1,9 +1,5 @@
 #Función resta, retorna la resta de 2 parámetros
-print("\n***Función resta***\n")
 
-def resta(num1, num2):
-
- print(int(num1) - int(num2))
-
-
-resta(num1=input("Ingrese un numero\n"), num2=input("Ingrese otro numero\n"))
+def resta(a, b):
+    print("\n***Función resta***")
+    return a-b
