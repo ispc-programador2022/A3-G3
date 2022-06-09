3c6187ff6f0f3099e912ebf8f5b79b144dc05a2f
Lucas Caminos <105889826+lucascaminosispc@users.noreply.github.com>
2022-06-09 12:49:24 -0300

    resta to main

--- A3G3.py
+++ A3G3.py
@@ -4,9 +4,11 @@ from Funciones.bienvenida import bienvenida
 from Funciones.ing2i import ing2i 
 from Funciones.suma import suma
 from Funciones.ing2s import ing2s
+from Funciones.resta import resta
 
 
 bienvenida()
 print("Los números ingresados son:", ing2i()) 
 print("Suma= ", suma())
 print("Cadenas= ",ing2s())
+resta()
