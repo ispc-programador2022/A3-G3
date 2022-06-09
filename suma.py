b75f306b318f2e3bb3e5f48ba41bc8b7ae63388a
ExComando <79213925+ExComando@users.noreply.github.com>
2022-06-08 22:18:51 -0300

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,11 @@
+# Función suma, retorna la suma de 2 parámetros.
+
+def suma(num1, num2):
+
+    print(int(num1)+ int(num2))
+
+
+suma(num1 = input("ingrese un numero\n"), num2 = input("ingrese otro numero\n"))
+
+
+
