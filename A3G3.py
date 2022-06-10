e4e1788a2fa4a7415b2aeb5f78e114486bc974b8
Camilo Sanchez <camilo_sanchezcjs29@hotmail.com>
2022-06-10 19:10:06 -0300

    Se agregaron las llamadas a las nuevas funciones

--- A3G3.py
+++ A3G3.py
@@ -7,6 +7,9 @@ from Funciones.ing2s import ing2s
 from Funciones.resta import resta
 from Funciones.producto import producto
 from Funciones.cociente import cociente
+from Funciones.modulo import modulo
+from Funciones.potencia import potencia
+from Funciones.radicacion import radicacion
 
 # Bienvenida
 bienvenida()
@@ -25,6 +28,15 @@ print("Producto = ", producto(nums[0], nums[1]))
 
 # Operación cociente
 print("Cociente = ", cociente(nums[0], nums[1]))
+
+# Operación módulo
+print("Módulo = ", modulo(nums[0], nums[1]))
+
+# Operación potencia
+print("Potencia = ", potencia(nums[0], nums[1]))
+
+# Operación radicación
+print("Raiz = ", radicacion(nums[0], nums[1]))
 
 
 # Ingreso de dos valores string
