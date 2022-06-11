9db6f6ae41ec63880f863f6748337d96ee117554
Camilo Sanchez <camilo_sanchezcjs29@hotmail.com>
2022-06-11 16:21:07 -0300

    Creación de las funciones p1_1, p1_2, p1_3

---
+++ Funciones/p1.py
@@ -0,0 +1,21 @@
+from Funciones.producto import producto
+from Funciones.suma import suma
+from Funciones.resta import resta
+
+# Función p1, retorna el producto de dos valores mas un tercer valor
+def p1_1(a, b, c):
+    prod = producto(a, b)
+    sum = suma(prod, c)
+    return sum
+
+# Función p2, retorna la suma de dos valores por un tercer valor
+def p1_2(a, b, c):
+    sum = suma(a, b)
+    prod = producto(sum, c)
+    return prod
+
+# Función p3, retorna la resta de dos valores por un tercer valor
+def p1_3(a, b, c):
+    res = resta(a, b)
+    prod = producto(res, c)
+    return prod
